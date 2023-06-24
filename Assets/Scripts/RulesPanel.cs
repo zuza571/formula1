fffbacf72b1584498903aa58b85b29feff94129a
jakubmazur965 <jakubmazur741@gmail.com>
2023-06-24 23:07:11 +0200

    Rules

---
+++ Assets/Scripts/RulesPanel.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class RulesPanel : MonoBehaviour
+{
+    public GameObject Rules;
+    private static bool isRules;
+
+    void Start()
+    {
+        Rules.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (isRules)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Rules.SetActive(true);
+        Time.timeScale = 0f;
+        isRules = true;
+    }
+
+    public void ResumeGame()
+    {
+        Rules.SetActive(false);
+        Time.timeScale = 1f;
+        isRules = false;
+    }
+
+}
+
